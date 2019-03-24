b75c0439febb482b5ad8676508e7022b58217af7
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2019-03-24 19:11:55 +0100

    0.1

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# WebF-CLI
+# 👩‍💻 WebF-CLI 👨‍💻
+
+Official WebF CLI
+
+## Usage
+
+#### Convert json file in webf file
+```bash
+./WebF-CLI.exe parseFile ./data.json ./data.webf
+```
+
+#### Convert webf file in json file
+```bash
+./WebF-CLI.exe decodeFile ./data.webf ./data.json
+```
+
+#### Convert json data in webf file
+```bash
+./WebF-CLI.exe parse {"title":"hello world !","meta": ["key":"desc","value"],"content":"..."} ./data.json
+```
+
+#### Convert webf data in json file
+```bash
+./WebF-CLI.exe decode [webf data] ./data.webf
+```
+
+#### Convert webf data in json data and get them
+```bash
+./WebF-CLI.exe decode [webf data]
+```
+
+#### Convert json data in webf data and get them
+```bash
+./WebF-CLI.exe parse {"title":"hello world !","meta": ["key":"desc","value"],"content":"..."}
+```
+
+#### Convert webf file in json data and get them
+```bash
+./WebF-CLI.exe decodeFile ./data.webf
+```
+
+#### Convert json file in webf data and get them
+```bash
+./WebF-CLI.exe parseFile ./data.json
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MPL](https://choosealicense.com/licenses/mpl-2.0/)
---
+++ cli.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"./WebF/src/utility"
+)
+
+func main() {
+	// WebF official Github repository : https://github.com/ThomasT404/WebF
+	// WebF-CLI official Github repository : https://github.com/ThomasT404/WebF-CLI
+	// WebF Version : 0.1 (WIP)
+	// WebF-CLI Version : 0.1 (WIP)
+	// Author : Thomas Tastet
+
+	argsWithoutProg := os.Args[1:]
+	lenn := len(argsWithoutProg)
+
+	if lenn == 0 {
+		utility.Error("You have not given any settings")
+	} else {
+		if lenn == 2 {
+			if argsWithoutProg[0] == "parseFile" {
+				fmt.Println(utility.JSONToWebF(utility.ReadJSONFromFile(argsWithoutProg[1])))
+			} else if argsWithoutProg[0] == "parse" {
+				fmt.Println(utility.JSONToWebF(utility.ReadJSON([]byte(argsWithoutProg[1]))))
+			} else if argsWithoutProg[0] == "decodeFile" {
+				fmt.Println(utility.WebFToJSON(utility.ReadWebFFromFile(argsWithoutProg[1])))
+			} else if argsWithoutProg[0] == "decode" {
+				fmt.Println(utility.WebFToJSON(utility.ReadWebF([]byte(argsWithoutProg[1]))))
+			}
+		} else if lenn == 3 {
+			if argsWithoutProg[0] == "parseFile" {
+				utility.ParseJSONToWebF(argsWithoutProg[1], argsWithoutProg[2])
+			} else if argsWithoutProg[0] == "parse" {
+				utility.ParseJSONToWebFFromString(argsWithoutProg[1], argsWithoutProg[2])
+			} else if argsWithoutProg[0] == "decodeFile" {
+				utility.ParseWebFToJSON(argsWithoutProg[1], argsWithoutProg[2])
+			} else if argsWithoutProg[0] == "decode" {
+				utility.ParseWebFToJSONFromString(argsWithoutProg[1], argsWithoutProg[2])
+			}
+		} else {
+			utility.Error("2 or 3 parameters expected")
+		}
+	}
+}
